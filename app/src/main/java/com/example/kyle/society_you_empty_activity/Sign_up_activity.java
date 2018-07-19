536bbe1f9a662f3ce75699b2f9ae2d4f72d6a9c3
omgKyleyay <1337scriptdoggy@gmail.com>
2018-07-19 02:35:19 -0400

    Initial commit

---
+++ app/src/main/java/com/example/kyle/society_you_empty_activity/Sign_up_activity.java
@@ -0,0 +1,13 @@
+package com.example.kyle.society_you_empty_activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sign_up_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_activity);
+    }
+}
